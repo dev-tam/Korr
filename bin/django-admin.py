1a1ed8c3a3ac80041d230b620eac9265d173a4a6
dev-tam <tamjid.mdh@gmail.com>
2018-01-17 05:11:15 +0600

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jtam/Development/Projects/Python/Django/url-shortener/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
